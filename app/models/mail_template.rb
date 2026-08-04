fb546dc650b41039836df9bc05f69f69d42fcdfa
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-08-04 19:58:40 +0900

    rubocop の指摘に対応した。

--- app/models/mail_template.rb
+++ app/models/mail_template.rb
@@ -4,7 +4,6 @@ class MailTemplate < RedmineMailTemplate::Utils::ModelBase
   belongs_to :project
   belongs_to :tracker
 
-  validates :project, presence: true
   validates :notifiable, presence: true
   validates :template, presence: true
 end
